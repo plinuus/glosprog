bee321786b9027301a388705d93866b23eae1c46
plinuus <linuscpettersson@gmail.com>
2015-12-15 10:41:05 +0100

    Påbörjade elevklassen

--- WindowsFormsApplication1/Model/Elev.cs
+++ WindowsFormsApplication1/Model/Elev.cs
@@ -8,5 +8,31 @@ namespace WindowsFormsApplication1.Model
 {
     class Elev
     {
+        private string namn;
+        private string klass;
+
+        public Elev(string u, string p)
+        {
+            this.namn = n;
+            this.klass = k;
+
+        }
+
+        public string Namn
+        {
+            get { return this.namn; }
+            set { this.namn = value; }
+        }
+
+        public string Password
+        {
+            get { return this.klass; }
+            set { this.klass = value; }
+        }
+
+        public override string ToString()
+        {
+            return (namn + "\t\t" + klass);
+        }
     }
 }
